ea2e7f2df07d1f460ad4bdc13936fe50c4e53e41
Leandro Franciscato <leandro@ingaflex.com.br>
2017-12-14 11:13:35 -0200

    Configuring Travis-ci

---
+++ push.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+setup_git() { 
+  git config --global user.email "leandro_franciscato@hotmail.com"
+  git config --global user.name "Leandro Franciscato"
+}
+
+commit_website_files() {  
+  git checkout master
+  git add .
+  git commit --message "Travis build"
+}
+
+upload_files() {
+  git remote add master https://${SOMEVAR}@github.com/LeandroFranciscato/30-seconds-of-code.git > /dev/null 2>&1
+  git push --quiet
+}
+
+setup_git
+commit_website_files
+upload_files
+
