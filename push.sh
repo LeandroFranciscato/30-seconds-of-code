663378a453d2ad72f24a8fb11a95ab9b83662646
Leandro Franciscato <leandro@ingaflex.com.br>
2017-12-14 11:58:18 -0200

    Travis

--- push.sh
+++ push.sh
@@ -12,7 +12,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add setup-ci https://${SOMEVAR}@github.com/LeandroFranciscato/30-seconds-of-code.git > /dev/null 2>&1
+  git remote add setup-ci https://LeandroFranciscato:${SOMEVAR}@github.com/LeandroFranciscato/30-seconds-of-code.git > /dev/null 2>&1
   git push --quiet
 }
 
